2b873febc4e345cc08aae8f01cddd978ed2c1123
Vernsz-Code <afycyt@gmail.com>
2024-05-06 15:21:58 +0700

    ini commit

--- lib/produkKeluarPage.dart
+++ lib/produkKeluarPage.dart
@@ -1,10 +1,16 @@
 import 'package:androidcokro/credit.dart';
 import 'package:androidcokro/produk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class produkKeluarPage extends StatelessWidget {
+class produkKeluarPage extends StatefulWidget {
   const produkKeluarPage({super.key});
 
+  @override
+  State<produkKeluarPage> createState() => _produkKeluarPageState();
+}
+
+class _produkKeluarPageState extends State<produkKeluarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,28 +68,9 @@ class produkKeluarPage extends StatelessWidget {
               title: const Text('Produk Keluar'),
               onTap: () {
                 Navigator.of(context).pop();
-                Navigator.push(
+                Navigator.pushReplacement(
                   context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) {
-                      return const produkKeluarPage();
-                    },
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      var begin = const Offset(1.0, 0.0);
-                      var end = Offset.zero;
-                      var curve = Curves.ease;
-                      var tween = Tween(begin: begin, end: end)
-                          .chain(CurveTween(curve: curve));
-                      var offsetAnimation = animation.drive(tween);
-
-                      return SlideTransition(
-                        position: offsetAnimation,
-                        child: child,
-                      );
-                    },
-                    transitionDuration: const Duration(milliseconds: 500),
-                  ),
+                  MaterialPageRoute(builder: (context) => produkKeluarPage()),
                 );
               },
             ),
@@ -91,7 +78,7 @@ class produkKeluarPage extends StatelessWidget {
               title: const Text('Produk'),
               onTap: () {
                 Navigator.of(context).pop();
-                Navigator.push(
+                Navigator.pushReplacement(
                   context,
                   PageRouteBuilder(
                     pageBuilder: (context, animation, secondaryAnimation) {
@@ -367,7 +354,7 @@ class produkKeluarPage extends StatelessWidget {
                 child: ElevatedButton(
                     onPressed: () {
                       Navigator.of(context).pop();
-                      Navigator.push(
+                      Navigator.pushReplacement(
                         context,
                         PageRouteBuilder(
                           pageBuilder:
